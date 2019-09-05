e74cb371c7f1abc83e2018e9c29555196edc2f67
bhaskarjha514 <bhaskarjha514>
2019-09-05 17:36:26 +0530

    first commit

---
+++ mac/shop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    
+    return render(request,'shop/index.html')
+
+def about(request):
+    return render(request,'shop/')
+
+def contact(request):
+    return render(request,'shop/')
+
+def tracker(request):
+    return render(request,'shop/')
+
+def search(request):
+    return render(request,'shop/')
+
+def prodView(request):
+    return render(request,'shop/')
+
+def checkout(request):
+    return render(request,'shop/')
+# Create your views here.
