7f8ced38c7d92c80a87a335217602ef51ecc2868
bhaskarjha514 <bhaskarjha514>
2019-09-05 22:56:56 +0530

    changes

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField
-    Product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50,default="")
     subcategory = models.CharField(max_length=50,default="")
     price = models.IntegerField(default=0)
@@ -12,5 +12,5 @@ class Product(models.Model):
     image = models.ImageField(upload_to="shop/images",default="")
 
     def __str__(self):
-        return self.Product_name
+        return self.product_name
     
